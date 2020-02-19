a554b0b86046523788c5d3b3a2061067b300242d
kingkrows <eiccheghrui@gmail.com>
2020-02-19 18:14:54 +0600

    Add AudioManager
    Testing Github

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    //audio manager singleton declearation
+    #region
+    public static AudioManager Instance;
+    private void Awake()
+    {
+        if(Instance==null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(Instance);
+        }
+    }
+    #endregion
+
+    
+
+
+
+}//audioManager class
